4a3cc41386d4603f2b1eb48d0b2f1caed306b3fc
Ramon <rvicentediaz@gmail.com>
2022-02-02 16:36:58 +0100

    Docker integration
    Dockerfile for the docker image, and a small script to run the image
    with the correct parameters and taking args into account (if you dont
    have the image it will download it)

---
+++ html2pdf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Small script to run docker with the required params for this image to work
+if [[ $(which docker) && $(docker --version) ]]; then
+    docker run -it \
+    --mount type=bind,source=$(pwd),target=/usr/src/html2pdf/input,bind-propagation=rslave \
+    -v $(pwd):/usr/src/html2pdf/output \
+    -u $(id -u):$(id -g) \
+    ramonvd/pdfconvert:latest "$@"
+else
+    echo "No tens Docker instal·lat. Cal que l'instal·lis primer, seguint les instruccions a: https://docs.docker.com/engine/install/ubuntu/"
+fi
+
+
