69faace122161cb4c52c0b921aceffcb9a1a3af4
ramonVD <rvicentediaz@gmail.com>
2022-02-03 14:49:34 +0100

    Update html2pdf.sh
    Changed propagation type, added read only

--- html2pdf.sh
+++ html2pdf.sh
@@ -2,12 +2,10 @@
 # Small script to run docker with the required params for this image to work
 if [[ $(which docker) && $(docker --version) ]]; then
     docker run -it \
-    --mount type=bind,source=$(pwd),target=/usr/src/html2pdf/input,bind-propagation=rslave \
+    --mount type=bind,source=$(pwd),target=/usr/src/html2pdf/input,bind-propagation=slave,ro \
     -v $(pwd):/usr/src/html2pdf/output \
     -u $(id -u):$(id -g) \
     ramonvd/pdfconvert:latest "$@"
 else
     echo "No tens Docker instal·lat. Cal que l'instal·lis primer, seguint les instruccions a: https://docs.docker.com/engine/install/ubuntu/"
 fi
-
-
